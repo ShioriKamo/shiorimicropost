715501e136194c42a0cbf062be8967748df88ed2
shiori.kamo <shiori.kamo@rakuten.com>
2018-06-19 04:00:22 +0000

    follow, unfollow

--- app/User.php
+++ app/User.php
@@ -27,8 +27,57 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
     
-     public function microposts()
+     public function microposts()//functionの後にあるのでmicropostsはインスタンスメソッド
     {
         return $this->hasMany(Micropost::class);
     }
+    
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
+    public function follow($userId)
+{
+    $exist = $this->is_following($userId);
+    
+    $its_me = $this->id == $userId;
+
+    if ($exist || $its_me) {
+    
+        return false;
+    } else {
+        
+        $this->followings()->attach($userId);
+        return true;
+    }
+}
+
+    public function unfollow($userId)
+{
+    // confirming if already following
+    $exist = $this->is_following($userId);
+    // confirming that it is not you
+    $its_me = $this->id == $userId;
+
+
+    if ($exist && !$its_me) {
+        // stop following if following
+        $this->followings()->detach($userId);
+        return true;
+    } else {
+        // do nothing if not following
+        return false;
+    }
+}
+
+
+    public function is_following($userId) {
+    return $this->followings()->where('follow_id', $userId)->exists();
+}
 }
